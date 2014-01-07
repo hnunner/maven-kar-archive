165370471910d4d126324c78b161b3cbe4bfc9c0
Hendrik Nunner <hendrik.nunner@next-audience.com>
2014-01-07 16:55:25 +0100

    use of same karaf instance for all tests, minor cleanups

--- itests/greeter/hello/src/test/java/com/adviser/maven/kar/itests/greeter/hello/HelloGreeterTest.java
+++ itests/greeter/hello/src/test/java/com/adviser/maven/kar/itests/greeter/hello/HelloGreeterTest.java
@@ -20,6 +20,8 @@ import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.karaf.options.LogLevelOption.LogLevel;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerSuite;
 import org.osgi.framework.BundleContext;
 import org.osgi.util.tracker.ServiceTracker;
 
@@ -30,6 +32,7 @@ import com.adviser.maven.kar.greeter.service.GreeterService;
  * @author h.nunner
  */
 @RunWith(PaxExam.class)
+@ExamReactorStrategy(value=PerSuite.class)
 public class HelloGreeterTest {
 
     /** logger */
@@ -62,8 +65,9 @@ public class HelloGreeterTest {
                         .frameworkUrl(
                                 maven().groupId("org.apache.karaf").artifactId("apache-karaf").type("zip").versionAsInProject())
                         .useDeployFolder(false).karafVersion("2.3.3").unpackDirectory(new File("target/pax/")),
-                // adds the key-value pairs to the defined configuration files
-//                editConfigurationFilePut("etc/system.properties", "firstProp", "value"),
+                // if local maven repository is not found in ~/.m2
+                // editConfigurationFilePut("org.ops4j.pax.url.mvn.cfg", "org.ops4j.pax.url.mvn.settings", "/home/hnunner/.m3/settings.xml"),
+                // editConfigurationFilePut("org.ops4j.pax.url.mvn.cfg", "org.ops4j.pax.url.mvn.localRepository", "/home/hnunner/.m3/repository"),
                 // keeps the runtime folder after finishing the tests
                 keepRuntimeFolder(),
                 // don't bother with local console output as it just ends up cluttering the logs
@@ -72,7 +76,6 @@ public class HelloGreeterTest {
                 logLevel(LogLevel.INFO),
                 // feature required by the following tests
                 features(
-//                        new RawUrlReference("target/classes/features.xml"),
                         maven().groupId("com.adviser.maven.kar").artifactId("maven-kar-archive-greeter-hello").type("xml")
                                 .versionAsInProject(),
                                 FEATURE_NAME)
@@ -111,7 +114,8 @@ public class HelloGreeterTest {
         serviceTracker.close();
 
         Assert.assertNotNull("No service registered: " + SERVICE_CLAZZ_NAME , greeterService);
-        Assert.assertEquals("Hello test", greeterService.greet("test"));
+        Assert.assertEquals("Unexpected message: " + greeterService.greet("test") + ", expected: Hello test",
+                "Hello test", greeterService.greet("test"));
 
         LOG.info("\n#######################\nService successfully installed: " + SERVICE_CLAZZ_NAME + "\n#######################");
     }
